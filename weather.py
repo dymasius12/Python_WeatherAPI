e24c67fa6498a6aaa79f02833857c193de6f6114
dymasius <dymasius12@gmail.com>
2022-09-13 23:21:49 +0800

    setting up some key, base_url and the response

--- weather.py
+++ weather.py
@@ -2,3 +2,9 @@ import requests
 
 
 API_KEY = "c1fb755a8d70f4cd9ff809c3359c0a3e"
+
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
